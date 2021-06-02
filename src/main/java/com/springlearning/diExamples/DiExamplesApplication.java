006576d7811396909751a68447678ed1b8796486
vivekjain3637 <vivekjain3637@gmail.com>
2021-06-02 14:58:20 +0530

    Initial Commit

---
+++ src/main/java/com/springlearning/diExamples/DiExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.diExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiExamplesApplication.class, args);
+	}
+
+}
